bd4310e6b2537c57f6759b2acd1c174f59018d4a
Yoshinori Kobayashi <yoshiiikoba@gmail.com>
2023-12-21 15:56:20 +1100

    TabVIewの中にTabViewを作りたい !

--- UpperTabView/ContentView.swift
+++ UpperTabView/ContentView.swift
@@ -24,10 +24,13 @@ struct ContentView: View {
             TabView(selection: $selectedTab,
                     content:  {
                 Text(TabState.post.rawValue + "画面").tabItem { Text("このテキストは見えない") }.tag(TabState.post)
-                Text(TabState.save.rawValue + "画面").tabItem { Text("このテキストは見えない") }.tag(TabState.save)
-                Text(TabState.setting.rawValue + "画面").tabItem { Text("このテキストは見えない") }.tag(TabState.setting)
+                SubView().tabItem { Text("このテキストは見えない") }.tag(TabState.save)
+                SubTabView().tabItem { Text("このテキストは見えない") }.tag(TabState.setting)
             })
             .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+
+            //            // 初めてTabViewを表示
+            //            SubTabView()
         }
     }
 
@@ -66,6 +69,39 @@ struct TopTabView: View {
         .shadow(color: .primary.opacity(0.2), radius: 3, x: 4, y: 4)
     }
 }
+
+struct SubView: View {
+    var body: some View {
+        ZStack {
+            Color.yellow
+            Text("普通のサブビュー")
+                .foregroundStyle(Color.black)
+        }
+    }
+}
+
+// 親がTabViewの場合は、このTabViewは表示されない
+struct SubTabView: View {
+
+    enum TabState2: String , CaseIterable{
+        case post = "投稿2"
+        case save = "保存2"
+        case setting = "設定2"
+    }
+
+    @State private var selectedTab2: TabState2 = .post
+
+    var body: some View {
+        TabView(selection: $selectedTab2,
+                content:  {
+            Text(TabState2.post.rawValue + "画面").tabItem { Text(TabState2.post.rawValue) }.tag(TabState2.post)
+            Text(TabState2.post.rawValue + "画面").tabItem { Text(TabState2.save.rawValue) }.tag(TabState2.post)
+            Text(TabState2.post.rawValue + "画面").tabItem { Text(TabState2.setting.rawValue) }.tag(TabState2.post)
+        })
+        .tabViewStyle(.page)
+    }
+}
+
 
 #Preview {
     ContentView()
